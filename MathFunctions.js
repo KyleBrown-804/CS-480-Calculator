682dc8317251b6568669a07d22fc10067b88111c
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2020-10-14 00:10:33 -0700

    Updated Input Field
    Input field will now only allow numbers, planning on switching to test for a regular expression allowing operators, and numbers

--- MathFunctions.js
+++ MathFunctions.js
@@ -1,7 +1,22 @@
-function clear() {
-}
+function clear() {}
 
 function test() {
-    alert("hello");
-    document.getElementById("math_input") = "test";
+  alert("hello");
+  document.getElementById("math_input") = "test";
+}
+
+function isValidKey(event) {
+  // create regular expression "reg" to allow 0-9, / , * , +, -, . , ^, ( , ),
+  // grab form string str
+
+  //alert(reg.test(str)); // use to test that it evaluates true/false correct
+  //return reg.test(str); // then this should either not allow, or allow those characters
+
+  const iKeyCode = event.which || event.keyCode;
+  if (iKeyCode >= 48 && iKeyCode <= 57) {
+    event;
+    return true;
+  } else {
+    return false;
+  }
 }
