f358ea52847cbd57394b5f9c84b8a0c544cd3929
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2020-10-18 17:20:40 -0700

    Keyboard Updates
    Keyboard is now interactable and values can be entered and viewed on the screen as well as clear and backspace functions working as intended

--- MathFunctions.js
+++ MathFunctions.js
@@ -1,7 +1,7 @@
 // Collecting HTML elements
-keyboard_elment = document.querySelector(".keyboard");
-user_input_element = document.querySelector(".user_input value");
-result_element = document.querySelector(".result value");
+const keyboard_element = document.querySelector(".keyboard");
+const user_input_element = document.querySelector(".user_input .value");
+const result_element = document.querySelector(".result .value");
 
 // Arrray of buttons names, symbols, evaluation equivalents, and type
 let calc_buttons = [
@@ -18,6 +18,18 @@ let calc_buttons = [
     type: "trig_func",
   },
   {
+    name: "all-clear",
+    symbol: "AC",
+    value: false,
+    type: "key",
+  },
+  {
+    name: "delete",
+    symbol: "←",
+    formula: false,
+    type: "key",
+  },
+  {
     name: "tan",
     symbol: "tan",
     value: "",
@@ -28,6 +40,18 @@ let calc_buttons = [
     symbol: "cot",
     value: "",
     type: "trig_func",
+  },
+  {
+    name: "open-paren",
+    symbol: "(",
+    formula: "(",
+    type: "number",
+  },
+  {
+    name: "close-paren",
+    symbol: ")",
+    formula: ")",
+    type: "number",
   },
   {
     name: "ln",
@@ -44,20 +68,8 @@ let calc_buttons = [
   {
     name: "exponent",
     symbol: "^",
-    value: "Math.pow", // POWER?
+    value: "Math.pow",
     type: "math_func",
-  },
-  {
-    name: "open-paren",
-    symbol: "(",
-    formula: "(",
-    type: "number",
-  },
-  {
-    name: "close-paren",
-    symbol: ")",
-    formula: ")",
-    type: "number",
   },
   {
     name: "divide",
@@ -66,70 +78,28 @@ let calc_buttons = [
     type: "operator",
   },
   {
+    name: "7",
+    symbol: "7",
+    value: "7",
+    type: "number",
+  },
+  {
+    name: "8",
+    symbol: "8",
+    value: "8",
+    type: "number",
+  },
+  {
+    name: "9",
+    symbol: "9",
+    value: "9",
+    type: "number",
+  },
+  {
     name: "multiply",
     symbol: "x",
     value: "*",
     type: "operator",
-  },
-  {
-    name: "add",
-    symbol: "+",
-    value: "+",
-    type: "operator",
-  },
-  {
-    name: "subtract",
-    symbol: "-",
-    value: "-",
-    type: "operator",
-  },
-  {
-    name: "decimal",
-    symbol: ".",
-    value: ".",
-    type: "number",
-  },
-  {
-    name: "delete",
-    symbol: "←",
-    formula: false,
-    type: "key",
-  },
-  {
-    name: "all-clear",
-    symbol: "AC",
-    value: false,
-    type: "key",
-  },
-  {
-    name: "calculate",
-    symbol: "=",
-    value: "=",
-    type: "calculate",
-  },
-  {
-    name: "0",
-    symbol: "0",
-    value: "0",
-    type: "number",
-  },
-  {
-    name: "1",
-    symbol: "1",
-    value: "1",
-    type: "number",
-  },
-  {
-    name: "2",
-    symbol: "2",
-    value: "2",
-    type: "number",
-  },
-  {
-    name: "3",
-    symbol: "3",
-    value: "3",
-    type: "number",
   },
   {
     name: "4",
@@ -150,42 +120,156 @@ let calc_buttons = [
     type: "number",
   },
   {
-    name: "7",
-    symbol: "7",
-    value: "7",
+    name: "subtract",
+    symbol: "-",
+    value: "-",
+    type: "operator",
+  },
+  {
+    name: "1",
+    symbol: "1",
+    value: "1",
     type: "number",
   },
   {
-    name: "8",
-    symbol: "8",
-    value: "8",
+    name: "2",
+    symbol: "2",
+    value: "2",
     type: "number",
   },
   {
-    name: "9",
-    symbol: "9",
-    value: "9",
+    name: "3",
+    symbol: "3",
+    value: "3",
     type: "number",
+  },
+  {
+    name: "add",
+    symbol: "+",
+    value: "+",
+    type: "operator",
+  },
+  {
+    name: "0",
+    symbol: "0",
+    value: "0",
+    type: "number",
+  },
+  {
+    name: "decimal",
+    symbol: ".",
+    value: ".",
+    type: "number",
+  },
+  {
+    name: "calculate",
+    symbol: "=",
+    value: "=",
+    type: "calculate",
   },
 ];
 
-let data = {
+let calc_data = {
   expression: [],
+  equiv_form: [],
 };
 
+// Function that neatly distributes buttons in rows
 function createKeyboard() {
-  const buttons_per_row = 5;
+  const buttons_per_row = 4;
   let added = 0;
 
   calc_buttons.forEach((button) => {
-    if (added == buttons_per_row) {
-      keyboard_elment.innerHTML += '<div class="row"></div>';
+    if (added % buttons_per_row == 0) {
+      keyboard_element.innerHTML += `<div class="row"></div>`;
     }
 
     const row = document.querySelector(".row:last-child");
-    row.innerHTML += '<button id="${button.name}">${button.symbol}</button>';
-    addded++;
+    row.innerHTML += `<button id="${button.name}">
+                      ${button.symbol}
+                      </button>`;
+    added++;
   });
+}
+
+// Populates keys across the keyboard
+createKeyboard();
+
+// Button Click Event Listener
+keyboard_element.addEventListener("click", (event) => {
+  const target_button = event.target;
+
+  calc_buttons.forEach((button) => {
+    if(button.name == target_button.id) {
+      onButtonPress(button);
+    }
+  });
+});
+
+// Button On Press Handler
+function onButtonPress(button) {
+  if(button.type == "operator") {
+
+    calc_data.expression.push(button.symbol);
+    calc_data.equiv_form.push(button.value);
+
+  } else if(button.type == "number") {
+
+    calc_data.expression.push(button.symbol);
+    calc_data.equiv_form.push(button.value);
+
+  } else if(button.type == "trig_func") {
+    
+  } else if(button.type == "mathy_func") {
+    
+  } else if(button.type == "key") {
+    
+    if(button.name == "all-clear") {
+
+      calc_data.expression = [];
+      calc_data.equiv_form = [];
+
+      updateResultToScreen(0);
+
+    } else if( button.name == "delete") {
+
+      calc_data.expression.pop();
+      calc_data.equiv_form.pop();
+
+    }
+  } else if(button.type == "calculate") {
+    
+    equiv_js_expression = calc_data.equiv_form.join('');
+
+    try {
+      let result = eval(equiv_js_expression);
+      updateResultToScreen(result);
+    }
+    catch(error) {
+      console.log(error);
+    }
+
+    // Testing syntax
+    
+    //checkSyntax();
+
+  }
+
+  updateInputToScreen(calc_data.expression.join(''));
+
+}
+
+// Updating the user input to the screen
+function updateInputToScreen(expression) {
+  user_input_element.innerHTML = expression;
+}
+
+function updateResultToScreen(result) {
+  result_element.innerHTML = result;
+}
+
+function checkSyntax() {
+  //return true / false?
 }
 
 // Caclulates the factorial of a number with a check to prevent overflow
@@ -200,20 +284,4 @@ function factorial(num) {
   }
 
   return fact;
-}
-
-function isValidKey(event) {
-  // create regular expression "reg" to allow 0-9, / , * , +, -, . , ^, ( , ),
-  // grab form string str
-
-  //alert(reg.test(str)); // use to test that it evaluates true/false correct
-  //return reg.test(str); // then this should either not allow, or allow those characters
-
-  const iKeyCode = event.which || event.keyCode;
-  if (iKeyCode >= 48 && iKeyCode <= 57) {
-    event;
-    return true;
-  } else {
-    return false;
-  }
 }
